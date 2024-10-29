dd14d0f839f08abfb285221877886ce2954337e7
Jan Philipp Knoeller <pk@pksoftware.de>
2024-10-29 16:59:19 +0100

    preview

--- bin/create-previews.sh
+++ bin/create-previews.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#PATH_TO_OPENSCAD="C:/'Program Files'/OpenSCAD/openscad.exe"
+PATH_TO_OPENSCAD="/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
+
 echo
 echo Creating preview images ...
 echo
@@ -10,14 +13,12 @@ cd "$(dirname "$0")"
 
 find ../sets -name "*.scad" -print0 | while read -d $'\0' file
 do
-    C:/'Program Files'/OpenSCAD/openscad --o "${file/scad/png}" --imgsize 1600,1200 --autocenter "$file"
+    "$PATH_TO_OPENSCAD" --o "${file/scad/png}" --imgsize 1600,1200 --autocenter "$file"
 done
 
 find ../examples -name "*.scad" -print0 | while read -d $'\0' file
 do
-    C:/'Program Files'/OpenSCAD/openscad --o "${file/scad/png}" --imgsize 1600,1200 --autocenter "$file"
+    "$PATH_TO_OPENSCAD" --o "${file/scad/png}" --imgsize 1600,1200 --autocenter "$file"
 done
 
 cd $curDir
-
-#
