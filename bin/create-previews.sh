cddef7ea9f96f479fb2591a59424def7ead1dfe7
pksoftware <info@pksoftware.de>
2025-02-01 18:32:37 +0100

    recreated preview images

--- bin/create-previews.sh
+++ bin/create-previews.sh
@@ -37,18 +37,17 @@ do
             label="$(basename ${file})"
             label=${label//-/ }
             label=${label/.scad/}
-            label=`python -c "print('${label}'[:28].title())"`
+            label=`python -c "print('${label}'[:32].title())"`
             image_file="${file/scad/png}"
 
             "$PATH_TO_OPENSCAD" --o "$image_file" --p "preview-parameters.json" --P BestQuality --csglimit 3000000 --imgsize ${IMAGE_WIDTH},${IMAGE_HEIGHT} --autocenter "$file"
             
             "$PATH_TO_MAGICK" \
-             \( "$image_file" -fill '#000000d0' -pointsize 140 -gravity northeast -bordercolor '#e5e5ce' -border ${IMAGE_BORDER} -font 'RBNo3.1-Book' -annotate '+120 +120' 'STL' \) \
-             \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 70 -gravity northeast -font 'RBNo3.1-Book' -annotate '+120 +280' "3D printable" \) \
+             \( "$image_file" -bordercolor '#e5e5ce' -border ${IMAGE_BORDER} \) \
              \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 140 -gravity northwest -font 'RBNo3.1-Book' -annotate '+120 +120' "${label}" \) \
-             \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 70 -gravity northwest -font 'RBNo3.1-Book' -annotate '+120 +280' "LEGO® compatible" \) \
+             \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 70 -gravity northwest -font 'RBNo3.1-Book' -annotate '+120 +290' "STL / 3MF Model" \) \
              \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 140 -gravity southwest -font 'RBNo3.1-Book' -annotate '+120 +120' "MachineBlocks" \) \
-             \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 70 -gravity southwest -font 'RBNo3.1-Book' -annotate '+120 +280' "generated with" \) \
+             \( -size ${IMAGE_WIDTH_FULL}x${IMAGE_HEIGHT_FULL} canvas:none -fill '#000000d0' -pointsize 70 -gravity southwest -font 'RBNo3.1-Book' -annotate '+120 +290' "generated with" \) \
              -layers flatten -resize ${IMAGE_WIDTH_HALF} -quality 80 "${file/scad/webp}"
 
              rm "$image_file"
